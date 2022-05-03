e1d29a95417ba937af7b4d2b6915fd7e4a3debb9
devguy201-9 <giathuan2388@gmail.com>
2022-05-03 18:44:49 +0700

    init project

---
+++ VegetableShop/Shared/products.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VegetableShop.Shared
+{
+    class products
+    {
+        public int id { get; set; }
+        public string product_code { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string images { get; set; }
+        public float price { get; set; }
+        public int quantity { get; set; }
+        public categories categories { get; set; }
+        public int category_id { get; set; }
+        public units units { get; set; }
+        public int unit_id { get; set; }
+        public int status { get; set; }
+        public DateTime created_at { get; set; } = DateTime.Now;
+        public DateTime updated_at { get; set; }
+    }
+}
---
+++ VegetableShop/Shared/units.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VegetableShop.Shared
+{
+    class units
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public List<products> products { get; set; }
+        public int status { get; set; }
+        public DateTime created_at { get; set; } = DateTime.Now;
+        public DateTime updated_at { get; set; }
+    }
+}
