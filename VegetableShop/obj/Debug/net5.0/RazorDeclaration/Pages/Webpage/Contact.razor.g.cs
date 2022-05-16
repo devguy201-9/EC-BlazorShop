6b7a4a55c377a7640d50629cf6ebd34919a7106f
devguy201-9 <giathuan2388@gmail.com>
2022-05-16 14:13:32 +0700

    update demo upload file

--- VegetableShop/obj/Debug/net5.0/RazorDeclaration/Pages/Webpage/Contact.razor.g.cs
+++ VegetableShop/obj/Debug/net5.0/RazorDeclaration/Pages/Webpage/Contact.razor.g.cs
@@ -4,7 +4,7 @@
 #pragma warning disable 0649
 #pragma warning disable 0169
 
-namespace VegetableShop.Shared
+namespace VegetableShop.Pages.Webpage
 {
     #line hidden
     using System;
@@ -82,28 +82,14 @@ using VegetableShop.Shared;
 #line default
 #line hidden
 #nullable disable
-    public partial class NavMenu : Microsoft.AspNetCore.Components.ComponentBase
+    [Microsoft.AspNetCore.Components.RouteAttribute("/Contact")]
+    public partial class Contact : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
-#nullable restore
-#line 44 "C:\Users\tgthuan\source\repos\VegetableShop\VegetableShop\Shared\NavMenu.razor"
-       
-    private bool collapseNavMenu = true;
-
-    private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
-
-    private void ToggleNavMenu()
-    {
-        collapseNavMenu = !collapseNavMenu;
-    }
-
-#line default
-#line hidden
-#nullable disable
     }
 }
 #pragma warning restore 1591
